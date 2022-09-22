a455fe7abc61c9da64e3684f17f8351808800461
tsung-han <d5269357812@gmail.com>
2022-09-22 16:31:21 +0800

    init

---
+++ randomPhoto/randomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomPhotoApp.swift
+//  randomPhoto
+//
+//  Created by tsung-han on 2022/9/14.
+//
+
+import SwiftUI
+
+@main
+struct randomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
