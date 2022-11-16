aa458e1e9fccb6bc14d1481479facf884deae11d
Jeffrey <d5269357812@gmail.com>
2022-11-16 17:06:47 +0800

    功能：Firestore 上傳圖片

--- randomPhoto/ContentView.swift
+++ randomPhoto/ContentView.swift
@@ -16,7 +16,7 @@ import FirebaseStorage
 @available(iOS 16.0, *)
 struct ContentView: View {
     @State var 動態圖片url="https://res.cloudinary.com/alchemyapi/image/upload/w_250,h_250/mainnet/07783aaf80e4c4497725b2da0af33394.png"
-    @State var photoModel:PhotoPickerModel = .init()
+    @StateObject var photoModel:PhotoPickerModel = .init()
     var body: some View {
         ZStack{
             Color.purple
@@ -24,20 +24,39 @@ struct ContentView: View {
             VStack{
                 Text("url:")
                 TextField("", text: $動態圖片url)
-                AsyncImage(url: URL(string: 動態圖片url))
+                //from 彼得潘 https://medium.com/%E5%BD%BC%E5%BE%97%E6%BD%98%E7%9A%84-swift-ios-app-%E9%96%8B%E7%99%BC%E5%95%8F%E9%A1%8C%E8%A7%A3%E7%AD%94%E9%9B%86/swiftui-%E9%A1%AF%E7%A4%BA%E7%B6%B2%E8%B7%AF%E5%9C%96%E7%89%87%E7%9A%84-asyncimage-7bbc93971e27
+                AsyncImage(url: URL(string: 動態圖片url)){ image in
+                    image
+                       .resizable()
+                } placeholder: {
+                    Color.gray
+                }
+                .scaledToFill()
+                .frame(width: 300, height: 300)
+                .clipShape(Circle())
                 PhotosPicker(selection: $photoModel.selectedPhoto,matching: .any(of: [.images])){
                     Text("選擇照片")
                 }
                 Image(uiImage: photoModel.uiImage)
+                    .resizable()
+                    .frame(width: 100, height: 100)
                 Button(action: {
-                    print("image is \( photoModel.uiImage )")
                     let storage = Storage.storage()
-                    storage.reference().child("temp.jpg").putData(photoModel.uiImage.jpegData(compressionQuality: 0.35)! , metadata: nil ){ (_ , err) in
+                    let riversRef = storage.reference().child("temp.jpg")
+                    riversRef.putData(photoModel.uiImage.jpegData(compressionQuality: 0.35)! , metadata: nil ){ (_ , err) in
                         if (err != nil){
                             print(err)
                             return
                         }
                         print("成功上傳")
+                        riversRef.downloadURL { (url, error) in
+                          guard let downloadURL = url else {
+                            // Uh-oh, an error occurred!
+                            return
+                          }
+                            動態圖片url = downloadURL.absoluteString
+                            photoModel.uiImage=UIImage()
+                        }
                     }
                 }, label: {
                     Text("上傳圖片")
@@ -47,13 +66,3 @@ struct ContentView: View {
         }
     }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        if #available(iOS 16.0, *) {
-//            ContentView()
-//        } else {
-//            // Fallback on earlier versions
-//        }
-//    }
-//}
